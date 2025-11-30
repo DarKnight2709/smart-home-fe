5652757ea53b5001e7f58cf8ebe928344579468b
quyentran <tranduyquyen1234567890@gmail.com>
2025-11-30 17:27:17 +0700

    authStore
    - tạo store nhờ zustand
    - store này lưu trữ thông tin của app (userId, roles...)
    - store lưu trữ dữ liệu lâu dài nhờ localStorage (nhấn F5 vẫn không mất dữ liệu).

---
+++ src/features/auth/stores/authStore.ts
@@ -0,0 +1,88 @@
+// Đây là Auth State Store được tạo bằng Zustand
+  // Lưu token + user info
+  // Decode token để lấy roles, username, userid
+  // Quản lý trạng thái login/ logout
+  // Lưu state vào localStorage để giữ đăng nhập sau F5
+  // Kết nối / ngắt kết nối socket khi login/logout.
+
+import { decodeToken } from '@/shared/lib/jwt'
+import ROUTES from '@/shared/lib/routes'
+import { useSocketStore } from '@/shared/stores/useSocketStore'
+import type { LoginResponse } from '@/shared/validations/AuthSchema'
+import { create } from 'zustand'
+import { createJSONStorage, devtools, persist } from 'zustand/middleware'
+
+
+// Định nghĩa shape của Auth State
+interface AuthState {
+  isAuth: boolean   
+  userId: string | null        // id trong jwt (decoded)
+  token: LoginResponse | null  // token được server trả (accessToken, refreshToken)
+  isLoading: boolean          // loading cho UI  
+  roles: string[] | null      // roles trong jwt (decoded)
+  username: string | null     // username trong jwt (decoded)
+  login: (token: LoginResponse) => Promise<void> // hàm login
+  logout: () => void           // hàm logout
+  setLoading: (loading: boolean) => void // hàm set loading
+}
+
+// tạo store Zustand
+const useAuthStore = create<AuthState>()(
+  devtools(
+    // lưu state vào localStorage
+    persist<AuthState>(
+      (set) => ({
+        isAuth: false,
+        userId: null,
+        token: null,
+        isLoading: false,
+        roles: null,
+        username: null,
+        // logic quan trọng nhất
+        // decode access token để lấy thông tin user
+        login: async (token: LoginResponse) => {
+          const decodedToken = decodeToken(token.accessToken)
+          // decode lỗi -> token rác -> logout
+          if (!decodedToken) {
+            window.location.href = ROUTES.LOGIN.url
+            return
+          }
+          // lưu thông tin vào store
+          // và nhờ persist tất cả được lưu vào localStorage
+          set({
+            isAuth: true,
+            token: token,
+            roles: decodedToken.roles,
+            userId: decodedToken.sub,
+            username: decodedToken.username
+          })
+
+          // Kết nối socket ngay sau khi login
+          // truyền accessToken vào socket để xác thực
+          const { connect } = useSocketStore.getState()
+          connect(token.accessToken)
+        },
+        logout: () => {
+          // ngắt kết nối socket 
+          const { disconnect } = useSocketStore.getState()
+          disconnect()
+
+          // xóa toàn bộ thông tin dữ liệu đăng nhập
+          // và nhờ persist tất cả được xóa khỏi localStorage
+          set({ isAuth: false, token: null, roles: null, userId: null, username: null })
+        },
+        // dùng để hiển thị loading cho UI
+        // VD: nút login, form,...
+        setLoading: (loading: boolean) => set({ isLoading: loading })
+      }),
+      // lưu vào localStorage với key 'auth-storage'
+      // F5 vẫn đăng nhập
+      {
+        name: 'auth-storage',
+        storage: createJSONStorage(() => localStorage)
+      }
+    )
+  )
+)
+
+export default useAuthStore
