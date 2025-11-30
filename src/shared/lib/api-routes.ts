6d118e7271669a01b07988626cb98c7e3fedfa29
quyentran <tranduyquyen1234567890@gmail.com>
2025-11-30 16:40:44 +0700

    api-routes

---
+++ src/shared/lib/api-routes.ts
@@ -0,0 +1,19 @@
+const API_ROUTES = {
+  AUTH: {
+    LOGIN: '/v1/auth/login',
+    REFRESH_TOKEN: '/v1/auth/refresh-token',
+    LOGOUT: '/v1/auth/logout'
+  },
+  ACADEMIC_YEARS: '/v1/academic-years',
+  FACULTIES: {
+    ROOT: '/v1/faculties',
+    MERGE: '/v1/faculties/merge'
+  },
+  USERS: '/v1/users',
+  ACCOUNTS: '/v1/accounts',
+  PERMISSIONS: '/v1/permissions',
+  ROLES: '/v1/roles',
+  AUDIT_LOGS: '/v1/audit-logs',
+}
+
+export default API_ROUTES
